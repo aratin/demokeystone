4672ddb43ca63da05410a5620cf135c704ec9c34
arati <arati.nankar@planertria.com>
2015-04-24 17:03:29 +0530

    change the data

--- public/js/lib/functional.js
+++ public/js/lib/functional.js
@@ -4,21 +4,38 @@
 //         $.print("#content-box");
 //     });
 
-    var doc = new jsPDF();
-        var specialElementHandlers = {
-            '#editor': function (element, renderer) {
-                return true;
-            }
-        };
+    $(document).ready(function(){ 
 
-        $('#pdf').on('click',function () {
-           // alert("asdfasdf");
-            doc.fromHTML($('#content-box').html(), 15, 15, {
-                'width': 170,
-                    'elementHandlers': specialElementHandlers
-            });
-            doc.save('sample-file.pdf');
-    });
+    var specialElementHandlers = {
+        '#editor': function (element,renderer) {
+            return true;
+        }
+    };
+ $('#pdf').click(function () {
+        var doc = new jsPDF();
+        doc.fromHTML($('#content-box').html(), 15, 15, {
+            'width': 170,'elementHandlers': specialElementHandlers
+        });
+        doc.save('sample-file.pdf');
+    });  
+});
+
+
+    // var doc = new jsPDF();
+    //     var specialElementHandlers = {
+    //         '#editor': function (element, renderer) {
+    //             return true;
+    //         }
+    //     };
+
+    //     $('#pdf').on('click',function () {
+    //        // alert("asdfasdf");
+    //         doc.fromHTML($('#content-box').html(), 15, 15, {
+    //             'width': 170,
+    //                 'elementHandlers': specialElementHandlers
+    //         });
+    //         doc.save('sample-file.pdf');
+    // });
 
       $(function() {
     $("#message").on('click', function() {
