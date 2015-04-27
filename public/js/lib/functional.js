5000117d896469370e4b7d240e6b594f2cc20cb2
arati <arati.nankar@planertria.com>
2015-04-27 17:37:39 +0530

    added files

--- public/js/lib/functional.js
+++ public/js/lib/functional.js
@@ -23,7 +23,7 @@
         console.log(imgData);
       // doc.setFontSize(40);
         //doc.text(35, 25, "#content-box");
-        //doc.addImage(imgData, 'JPEG', 15, 40, 100, 50);
+        doc.addImage(imgData, 'JPEG', 15, 40, 80, 80);
         doc.fromHTML($('#content-box').html(), 15, 15, {
              'width': 170,'elementHandlers': specialElementHandlers
          });
