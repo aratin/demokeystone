a6196773b3b5b30dd13b5ca0ec5d607470b4be3c
arati <arati.nankar@planertria.com>
2015-04-27 14:07:14 +0530

    added files

--- public/js/lib/functional.js
+++ public/js/lib/functional.js
@@ -12,37 +12,16 @@
         }
     };
  $('#pdf').click(function () {
-        var imgData = 'data:image/jpeg;base64,';
         var doc = new jsPDF();
-
-        doc.setFontSize(40);
-        doc.text(35, 25, "Octonyan loves jsPDF");
-        doc.addImage(imgData, 'JPEG', 15, 40, 180, 180);
-
-        doc.addHTML($('#content-box'), 15, 15, {
+        doc.fromHTML($("#image").css("background-color", "red"), 15, 15, {
             'width': 170,'elementHandlers': specialElementHandlers
         });
-
         doc.save('sample-file.pdf');
     });  
 });
 
 
-    // var doc = new jsPDF();
-    //     var specialElementHandlers = {
-    //         '#editor': function (element, renderer) {
-    //             return true;
-    //         }
-    //     };
-
-    //     $('#pdf').on('click',function () {
-    //        // alert("asdfasdf");
-    //         doc.fromHTML($('#content-box').html(), 15, 15, {
-    //             'width': 170,
-    //                 'elementHandlers': specialElementHandlers
-    //         });
-    //         doc.save('sample-file.pdf');
-    // });
+    
 
       $(function() {
     $("#message").on('click', function() {
