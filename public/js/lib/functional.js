f173f4be745978a252a6d6239cc1ced198b8c269
arati <arati.nankar@planertria.com>
2015-04-27 15:45:15 +0530

    added files

--- public/js/lib/functional.js
+++ public/js/lib/functional.js
@@ -21,9 +21,12 @@
     };
  $('#pdf').click(function () {
         var imageSrc = $('#image').attr('src');
+        
+        console.log(imageSrc);
         var doc = new jsPDF();
 
         var imgData = 'data:image/jpeg;base64,/imageSrc';
+        console.log(imgData);
         doc.setFontSize(40);
         doc.text(35, 25, "Octonyan loves jsPDF");
         doc.addImage(imgData, 'JPEG', 15, 40, 180, 180);
