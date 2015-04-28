224f7c74c2f4c8e90ff67d3dfbb9cfb43e5e8030
arati <arati.nankar@planertria.com>
2015-04-28 11:18:25 +0530

    added data

--- public/js/lib/functional.js
+++ public/js/lib/functional.js
@@ -50,7 +50,7 @@ $('#pdf').click(function () {
        // doc.setFontSize(40);
         //doc.text(10, 20, "Software products");
         doc.addImage(imgData, 'JPEG', 15, 15, 50, 50);
-        doc.fromHTML($('#content-box').html(), 50, 50, {
+        doc.fromHTML($('#content-box').html(), 80, 80, {
              'width': 150,'elementHandlers': specialElementHandlers
         });
         doc.save('sample-file.pdf');
