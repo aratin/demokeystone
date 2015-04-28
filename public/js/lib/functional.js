476de827ae4c7ce48e259d88c972f3fb689081dc
arati <arati.nankar@planertria.com>
2015-04-28 13:18:49 +0530

    change the data

--- public/js/lib/functional.js
+++ public/js/lib/functional.js
@@ -51,7 +51,7 @@ $('#pdf').click(function () {
         //doc.text(10, 20, "Software products");
         //doc.addImage(imgData, 'JPEG', 15, 15, 50, 50);
         doc.setFontSize(25);
-        doc.fromHTML($(this).siblings('.col-md-9').html(), 15, 15, {
+        doc.fromHTML($('#content-box').html(), 15, 15, {
              'width': 170,'elementHandlers': specialElementHandlers
         });
         doc.save('sample-file.pdf');
